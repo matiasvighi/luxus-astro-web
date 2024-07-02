133f6c25ddf4216418f150ca955df99a2c7df324
ccarapezza <carapezza.christian@gmail.com>
2024-07-02 03:20:32 -0300

    Listos todos los componentes y la version mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-        colors: {
-            ...colors,
-            luxus: '#ff135a',
-            luxusBackground: '#343a40',
+		extend: {
+            colors: {
+                luxus: '#ff135a',
+                luxusBackground: '#343a40',
+            },
+            boxShadow: {
+                'up-down': '26px -4px 26px rgba(0, 0, 0, 0.1)',
+            }
         },
-		extend: {},
 	},
 	plugins: [],
 }
